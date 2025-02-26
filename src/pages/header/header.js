6e990eab801bb7ad7bdc06f27cf88b90b290dbc1
lucasns06 <lucasnascimento.carreira@gmail.com>
2025-02-26 11:38:13 -0300

    ✏️ logo header nome

--- src/pages/header/header.js
+++ src/pages/header/header.js
@@ -52,13 +52,14 @@ const Header = () => {
                 <div className="fixed inset-0 z-50" />
                 <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
+                        <a href="#" className="-m-1.5 p-1.5 flex items-center gap-2">
                             <span className="sr-only">Your Company</span>
                             <img
                                 alt=""
                                 src="study_icon.png"
                                 className="h-8 w-auto"
                             />
+                            <p className='text-blue-500 font-bold'>EstudosAPI</p>
                         </a>
                         <button
                             type="button"
